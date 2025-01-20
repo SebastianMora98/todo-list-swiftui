f2a7320d32352702d42aa6adad829c0d27fe0fc0
jmoraautomatiza <jmora@automatiza.co>
2025-01-20 01:04:22 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Jhon Sebastian Mora Orozco on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
